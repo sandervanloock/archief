79997a3599b2072b4e73cf4afecdc7ca4dba16e2
Sander <sander@sander-laptop.(none)>
2013-12-28 13:35:20 +0100

    removed src folder

--- front/src/common/resources/events.js
+++
@@ -1,9 +0,0 @@
-angular.module('resources.events', ['mysqlResource']);
-angular.module('resources.events')
-
-.factory('Events', ['mysqlResource', function ($mysqlResource) {
-
-  var Events = $mysqlResource('events');
-
-  return Events;
-}]);
