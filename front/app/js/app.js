af2e18d0bdf13387f6fb3681f47a5d46939187b9
Sander Van Loock <svl@foreach.be>
2014-01-20 23:43:44 +0100

    apparte properties file

--- front/app/js/app.js
+++ front/app/js/app.js
@@ -1,32 +1,23 @@
-var app = angular
-		.module('app', 
-				[ 'ngRoute', 'appControllers', 'eventService', 'photoService','security','arrayFilters','ui.bootstrap', 'angularSpinner' ]);
+var app = angular.module('app',
+        [ 'ngRoute', 'appControllers', 'eventService', 'photoService', 'users','security', 'arrayFilters', 'ui.bootstrap', 'angularSpinner' ]);
 
-angular.module('app').constant('ARCHIVE_SERVER_CONFIG',
-		'http://localhost/Chiro/archief/backend/public/');
-
-angular.module('app').constant('STATIC_SERVER_CONFIG',
-	'http://localhost/Chiro/archief/');
-
-angular.module('app').run(['security', function(security) {
-	  // Get the current user when the application starts
-	  // (in case they are still logged in from a previous session)
-	  security.requestCurrentUser();
+angular.module('app').run(['security', function (security) {
+    // Get the current user when the application starts
+    // (in case they are still logged in from a previous session)
+    security.requestCurrentUser();
 }]);
 
-app.config([ '$routeProvider', 'securityAuthorizationProvider', function($routeProvider, securityAuthorizationProvider) {
-	$routeProvider.when('/events', {
-		templateUrl : 'template/event-list.html',
-		controller : 'EventListCtrl',
-		resolve: securityAuthorizationProvider.requireAdminUser
-	}).when('/events/:eventId', {
-		templateUrl : 'template/event-detail.html',
-		controller : 'EventDetailCtrl',
-		resolve: securityAuthorizationProvider.requireAdminUser
-	}).when('/home',{
-		templateUrl : 'template/archief.html',
-		controller : 'ArchiefCtrl'
-	}).otherwise({
-		redirectTo : '/home'
-	});
+app.config([ '$routeProvider', 'securityAuthorizationProvider', function ($routeProvider, securityAuthorizationProvider) {
+    $routeProvider.when('/events', {
+        templateUrl: 'template/event-list.html',
+        controller: 'EventListCtrl',
+        resolve: securityAuthorizationProvider.requireAdminUser
+    }).when('/events/:eventId', {
+            templateUrl: 'template/event-detail.html',
+            controller: 'EventDetailCtrl',
+            resolve: securityAuthorizationProvider.requireAdminUser
+        }).when('/home', {
+            templateUrl: 'template/archief.html',
+            controller: 'ArchiefCtrl'
+        })
 } ]);
--- front/app/js/properties.js
+++ front/app/js/properties.js
@@ -1,2 +1,8 @@
 var backendHost = 'http://localhost/Chiro/archief/backend';
 var staticHost = 'http://chiroelzestraat.be/archief/data';
+
+angular.module('app').constant('ARCHIVE_SERVER_CONFIG',
+    'http://localhost:8081/backend/public/');
+
+angular.module('app').constant('STATIC_SERVER_CONFIG',
+    'http://localhost:8081/');
