322bee9b64f1b42e9863d0e43f67fc16db5153ff
Sander Van Loock <svl@foreach.be>
2014-01-22 19:55:43 +0100

    users detail pagina

--- front/app/js/users/users.js
+++ front/app/js/users/users.js
@@ -1,27 +1,42 @@
-angular.module('users', ['userService','security.authorization'])
+angular.module('users', ['userService', 'security.authorization'])
 
     .config(['$routeProvider', 'securityAuthorizationProvider', function ($routeProvider, securityAuthorizationProvider) {
         $routeProvider.when('/users', {
-            templateUrl:'js/users/user-list.html',
-            controller:'UsersViewCtrl',
-            resolve:{
-                users:['Users', function (Users) {
+            templateUrl: 'js/users/user-list.html',
+            controller: 'UsersViewCtrl',
+            resolve: {
+                users: ['Users', function (Users) {
                     return Users.query();
                 }],
                 authenticatedUser: securityAuthorizationProvider.requireAuthenticatedUser
             }
-        });
+        }).when('/users/:userId', {
+                templateUrl: 'js/users/user-detail.html',
+                controller: 'UserDetailCtrl',
+                resolve: securityAuthorizationProvider.requireAdminUser
+            });
     }])
 
-    .controller('UsersViewCtrl', ['$scope', 'users',function ($scope, users) {
+    .controller('UsersViewCtrl', ['$scope', '$location', 'users', function ($scope, $location, users) {
         $scope.users = users;
+        $scope.createUser = function () {
+            $location.path("/users/-1");
+        }
+
+        $scope.editUser = function (id) {
+            $location.path("/users/" + id);
+        }
+    }])
+
+    .controller('UserDetailCtrl', ['$scope', '$routeParams', 'Users', function ($scope,  $routeParams,  Users) {
+        $scope.user = Users.get({id: $routeParams.userId})
     }]);
 
 angular.module('userService', ['ngResource']);
 
 angular.module('userService').factory('Users', ['$resource', 'ARCHIVE_SERVER_CONFIG',
-    function($resource, ARCHIVE_SERVER_CONFIG){
+    function ($resource, ARCHIVE_SERVER_CONFIG) {
         return $resource(ARCHIVE_SERVER_CONFIG + 'users', {}, {
-            query: {method:'GET', isArray:true}
+            query: {method: 'GET', isArray: true}
         });
     }]);
