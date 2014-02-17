886bad5938c82e8ccc5131c8e6ea5b42b779ada4
Sander Van Loock <svl@foreach.be>
2014-02-17 13:38:48 +0100

    image choser for sponsor

--- rommelmarkt/app/scripts/app.js
+++ rommelmarkt/app/scripts/app.js
@@ -22,4 +22,20 @@ angular.module('rommelmarktApp', [
             });
     });
 
+angular.module('rommelmarktApp').directive("ngFileSelect",function(){
+
+    return {
+        link: function($scope,el){
+
+            el.bind("change", function(e){
+
+                $scope.file = (e.srcElement || e.target).files[0];
+                $scope.getFile();
+            })
+
+        }
+
+    }
+})
+
 
--- rommelmarkt/app/scripts/controllers/main.js
+++ rommelmarkt/app/scripts/controllers/main.js
@@ -7,8 +7,19 @@ angular.module('rommelmarktApp')
         } ]);
 
 angular.module('rommelmarktApp')
-    .controller('SponsorCtrl', [ '$scope', '$location','Sponsor',
-        function($scope, $location, Sponsor) {
+    .controller('SponsorCtrl', [ '$scope', '$location','Sponsor', 'fileReader',
+        function($scope, $location, Sponsor, fileReader) {
             $scope.sponsor = {};
+            $scope.getFile = function () {
+                $scope.progress = 0;
+                fileReader.readAsDataUrl($scope.file, $scope)
+                    .then(function(result) {
+                        $scope.imageSrc = result;
+                    });
+            };
+
+            $scope.$on("fileProgress", function(e, progress) {
+                $scope.progress = progress.loaded / progress.total;
+            });
         } ]);
 
