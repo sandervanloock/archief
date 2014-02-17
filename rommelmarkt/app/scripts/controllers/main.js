982f60a5611078724b4d774e4b268bf4a9569131
Sander Van Loock <svl@foreach.be>
2014-02-17 20:06:51 +0100

    post sponsors

--- rommelmarkt/app/scripts/controllers/main.js
+++ rommelmarkt/app/scripts/controllers/main.js
@@ -7,8 +7,8 @@ angular.module('rommelmarktApp')
         } ]);
 
 angular.module('rommelmarktApp')
-    .controller('SponsorCtrl', [ '$scope', '$location','Sponsor', 'fileReader',
-        function($scope, $location, Sponsor, fileReader) {
+    .controller('SponsorCtrl', [ '$scope', '$location','$http', 'fileReader', 'BACKEND_SERVER_CONFIG',
+        function($scope, $location, $http, fileReader, BACKEND_SERVER_CONFIG) {
             $scope.sponsor = {};
             $scope.getFile = function () {
                 $scope.progress = 0;
@@ -21,5 +21,37 @@ angular.module('rommelmarktApp')
             $scope.$on("fileProgress", function(e, progress) {
                 $scope.progress = progress.loaded / progress.total;
             });
+            $scope.submitSponsor = function(){
+                var sponsor = $scope.sponsor;
+                if(sponsor.amount!=0){
+                    sponsor.object = "CASH";
+                    switch(sponsor.amount){
+                        case "50":
+                            sponsor.dimension = 1;
+                            break;
+                        case "100":
+                            sponsor.dimension = 2;
+                            break;
+                        otherwise:
+                            sponsor.dimension = -1;
+                    }
+                }
+                $http({
+                    method  : 'POST',
+                    url     : BACKEND_SERVER_CONFIG + 'sponsor/add',
+                    data    : $.param(sponsor),  // pass in data as strings
+                    headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+                }).success(function(data) {
+                        console.log(data);
+                        if (!data.success) {
+                            // if not successful, bind errors to error variables
+                            $scope.errorName = data.errors.name;
+                            $scope.errorSuperhero = data.errors.superheroAlias;
+                        } else {
+                            // if successful, bind success message to message
+                            $scope.message = data.message;
+                        }
+                    });
+            }
         } ]);
 
--- rommelmarkt/app/scripts/services/main.js
+++ rommelmarkt/app/scripts/services/main.js
@@ -2,7 +2,13 @@ var sponsorService = angular.module('sponsorService', ['ngResource']);
 
 sponsorService.factory('Sponsor', ['$resource', 'BACKEND_SERVER_CONFIG',
     function ($resource,BACKEND_SERVER_CONFIG) {
+        var xsrf = $.param({fkey: "key"});
         return $resource(BACKEND_SERVER_CONFIG + 'sponsor', {}, {
-            query: {method: 'GET', isArray: true}
+            query: {method: 'GET', isArray: true},
+            save: {
+                method: 'POST',
+                url: BACKEND_SERVER_CONFIG + 'sponsor',
+                data: xsrf,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}}
         });
     }]);
