e4fcafa9d04f00fc45fdee7c140ac8c1b72878fc
Sander Van Loock <svl@foreach.be>
2014-04-19 17:10:51 +0200

    timeline settings en GA activation

--- front/app/js/controllers.js
+++ front/app/js/controllers.js
@@ -1,30 +1,30 @@
 var appControllers = angular.module('appControllers', []);
 
 appControllers.controller('EventListCtrl', [ '$scope', '$location', 'Event', 'configuration',
-    function ($scope, $location, Event,configuration) {
+    function ($scope, $location, Event, configuration) {
         $scope.staticServer = configuration.STATIC_SERVER_CONFIG;
         $scope.events = Event.query({
             type: $location.search().type
         });
         $scope.orderProp = 'start';
 
-        $scope.create = function(){
+        $scope.create = function () {
             $location.path("/event/new");
         }
     } ]);
 
-appControllers.controller('EventCreateCtrl',[ '$scope', 'Event', 'configuration',
-    function ($scope, Event,configuration) {
+appControllers.controller('EventCreateCtrl', [ '$scope', 'Event', 'configuration',
+    function ($scope, Event, configuration) {
         $scope.event = {};
         $scope.dateFormat = "dd/MM/yyyy";
-        $scope.create = function(){
+        $scope.create = function () {
             Event.create($scope.event);
         };
         $scope.dateOptions = {
             'year-format': "'yyyy'",
             'starting-day': 1,
         };
-        $scope.open = function($event, opened) {
+        $scope.open = function ($event, opened) {
             $event.preventDefault();
             $event.stopPropagation();
 
@@ -57,7 +57,7 @@ appControllers.controller('EventDetailCtrl', [
         var spinner = new Spinner({top: '50px'}).spin($("#spinner-container")[0]);
         Event.get({
             id: $routeParams.eventId
-        },function (data) {
+        }, function (data) {
             spinner.stop();
             $scope.event = data.event;
 //            reformat the date to a js date so the directive works :)
@@ -77,10 +77,10 @@ appControllers.controller('EventDetailCtrl', [
             $scope.photos = photoArrays;
         });
 
-        $scope.save = function(){
+        $scope.save = function () {
             $scope.event.start = moment($scope.event.start).format("YYYY-MM-DD HH:mm:ss");
             $scope.event.end = moment($scope.event.end).format("YYYY-MM-DD HH:mm:ss");
-            Event.update({eventId: $scope.event.id},$scope.event);
+            Event.update({eventId: $scope.event.id}, $scope.event);
         };
 
         $scope.incrementPage = function () {
@@ -99,7 +99,7 @@ appControllers.controller('EventDetailCtrl', [
         $scope.changeEvent = function () {
             $location.path("/photos/" + $scope.event);
         };
-        $scope.delete = function(){
+        $scope.delete = function () {
             Event.remove({eventId: $scope.event.id});
             $location.path("/events");
         }
@@ -123,7 +123,7 @@ appControllers.controller('PhotoCtrl', [
     'photoService',
     'selectedPhoto',
     'configuration',
-    function ($scope, $modalInstance, $http, photoService,selectedPhoto, configuration) {
+    function ($scope, $modalInstance, $http, photoService, selectedPhoto, configuration) {
         $scope.photo = selectedPhoto;
         $scope.staticServer = configuration.STATIC_SERVER_CONFIG;
         $scope.toggleState = function () {
@@ -136,12 +136,12 @@ appControllers.controller('PhotoCtrl', [
             $http({
                 url: configuration.ARCHIVE_SERVER_CONFIG + 'photo/' + $scope.photo.id,
                 method: 'PUT',
-                data:  $.param({
+                data: $.param({
                     id: $scope.photo.id,
                     event: $scope.photo.event,
                     title: $scope.photo.title,
                     directory: $scope.photo.directory,
-                    live: $scope.photo.live  == "1" ? 1 : 0,
+                    live: $scope.photo.live == "1" ? 1 : 0,
                     deleted: $scope.photo.deleted == "1" ? 1 : 0,
                     created: $scope.photo.created,
                     modified: moment().format("YYYY-MM-DD HH:mm:ss")
@@ -170,45 +170,56 @@ appControllers.controller('ArchiefCtrl', [
     'configuration',
     'Event',
     function ($scope, configuration, Event) {
+        var spinner = new Spinner({top: '250px'}).spin($("#timeline")[0]);
         Event.query(function (events) {
-                var timeline = {
-                    timeline: {
-                        header: "Digitaal archief - Chiro Elzestraat",
-                        type: "default",
-
-                        text: "",
-                        date: []
-                    }
-                };
-                for (var i = 0; i < events.length; i++) {
-                    timeline.timeline.date
-                        .push({
-                            startDate: moment(events[i].start).format("YYYY,MM,DD"),
-                            endDate: moment(events[i].end).format("YYYY,MM,DD"),
-                            headline: events[i].name,
-                            text: events[i].description,
-                            asset: {
-                                "media": createNivoSlider(events[i].photos)
-                            }
-                        });
+            spinner.stop();
+            var timeline = {
+                timeline: {
+                    header: "Digitaal archief - Chiro Elzestraat",
+                    type: "default",
+                    text: "",
+                    date: [],
+                    era: []
                 }
-                createStoryJS({
-                    type: 'timeline',
-                    source: timeline,
-                    embed_id: 'timeline',
-                    debug: false,
-                    css: 'css/vendor/timeline/timeline.css',
-                    js: 'js/vendor/timeline/timeline.js'
-                });
+            };
+            for (var i = 0; i < events.length; i++) {
+                timeline.timeline.date
+                    .push({
+                        startDate: moment(events[i].start).format("YYYY,MM,DD"),
+                        endDate: moment(events[i].end).format("YYYY,MM,DD"),
+                        headline: events[i].name,
+                        text: events[i].description,
+                        asset: {
+                            "media": createNivoSlider(events[i].photos)
+                        }
+                    });
+//                timeline.timeline.era.push({
+//                    "startDate":"2011,09,01",
+//                    "endDate":"2012,05,30",
+//                    "headline":"Chirojaar 2011 - 2012",
+//                    "text":"<p>Body text goes here, some HTML is OK</p>",
+//                    "tag":"This is Optional"
+//                })
+            }
+            createStoryJS({
+                type: 'timeline',
+                source: timeline,
+                embed_id: 'timeline',
+                debug: false,
+                css: 'css/vendor/timeline/timeline.css',
+                js: 'js/vendor/timeline/timeline.js',
+                lang: 'nl',
+                start_at_end:true
             });
+        });
 
         createNivoSlider = function (photos) {
-            if(photos.length==0){
+            if (photos.length == 0) {
                 return "";
             }
             var html = "<div class='slider-wrapper theme-default'><div class='ribbon'></div><div class='nivoSlider'>";
             for (var i = 0; i < photos.length; i++) {
-            var photoDir = configuration.STATIC_SERVER_CONFIG + "data/" + photos[i].directory;
+                var photoDir = configuration.STATIC_SERVER_CONFIG + "data/" + photos[i].directory;
                 html += "<a class='popup-image' href='" + photoDir + "'>" +
                     "<img src='" + photoDir
                     + "' alt='" + photos[i].title
