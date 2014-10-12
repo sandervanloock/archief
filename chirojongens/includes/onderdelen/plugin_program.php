c02f721173130416ae86c9005c5e1e6b7143bb93
Sander Van Loock <svl@foreach.be>
2014-10-12 16:39:45 +0200

    styling download links

--- chirojongens/includes/onderdelen/plugin_program.php
+++ chirojongens/includes/onderdelen/plugin_program.php
@@ -1,21 +1,22 @@
-<a href="http://get.adobe.com/nl/reader/" >
-<img src="images/pdf_icon.png" class="pdfimg" height="60" alt="Kampboekje"/></a>
-<strong class="orangeTitle" style="margin-top:3px; ">'t Program<br /><h5>September - December 2014</h5></strong>
-<a href="https://docs.google.com/gview?url=http://chirojongens.chiroelzestraat.be/uploads/ProgramSeptemberDecember2014.pdf?embedded=true" onclick="return popup(this, 'notes')" style="float:left;">Bekijken</a>
-<a href="uploads/ProgramSeptemberDecember2014.pdf" style="padding-right:15px;float:right;">Downloaden</a>
-<!--Rechtsklik -> Opslaan als...-->
+<a href="http://get.adobe.com/nl/reader/">
+    <img src="images/pdf_icon.png" class="pdfimg" height="60" alt="Kampboekje"/></a>
+<strong class="orangeTitle" style="margin-top:3px; ">'t Program<br/><h5>September - December 2014</h5></strong>
+<div class="program-links">
+    <a href="https://docs.google.com/gview?url=http://chirojongens.chiroelzestraat.be/uploads/ProgramSeptemberDecember2014.pdf?embedded=true"
+       onclick="return popup(this, 'notes')">Bekijken</a>
+    <a href="uploads/ProgramSeptemberDecember2014.pdf">Downloaden</a>
+</div>
 <script type="text/javascript">
-<!--
-function popup(mylink, windowname)
-{
-if (! window.focus)return true;
-var href;
-if (typeof(mylink) == 'string')
-   href=mylink;
-else
-   href=mylink.href;
-window.open(href, windowname, 'width=800,height=600,scrollbars=yes');
-return false;
-}
-//-->
+    <!--
+    function popup(mylink, windowname) {
+        if (!window.focus)return true;
+        var href;
+        if (typeof(mylink) == 'string')
+            href = mylink;
+        else
+            href = mylink.href;
+        window.open(href, windowname, 'width=800,height=600,scrollbars=yes');
+        return false;
+    }
+    //-->
 </script>
