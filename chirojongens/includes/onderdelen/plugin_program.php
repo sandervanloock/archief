298c01767bee919373f70d1a02a798cc9e88db0b
Sander Van Loock <svl@foreach.be>
2014-10-12 16:50:39 +0200

    styling download links 2

--- chirojongens/includes/onderdelen/plugin_program.php
+++ chirojongens/includes/onderdelen/plugin_program.php
@@ -1,10 +1,24 @@
-<a href="http://get.adobe.com/nl/reader/">
-    <img src="images/pdf_icon.png" class="pdfimg" height="60" alt="Kampboekje"/></a>
-<strong class="orangeTitle" style="margin-top:3px; ">'t Program<br/><h5>September - December 2014</h5></strong>
-<div class="program-links">
-    <a href="https://docs.google.com/gview?url=http://chirojongens.chiroelzestraat.be/uploads/ProgramSeptemberDecember2014.pdf?embedded=true"
-       onclick="return popup(this, 'notes')">Bekijken</a>
-    <a href="uploads/ProgramSeptemberDecember2014.pdf">Downloaden</a>
+<div class="program-container">
+    <a href="http://get.adobe.com/nl/reader/">
+        <img src="images/pdf_icon.png" class="pdfimg" height="60" alt="Kampboekje"/></a>
+
+    <div class="program-date-container">
+        <strong class="program-title orangeTitle">
+            <div class="program-date">
+                <div class="program-from">
+                    September 2014
+                </div>
+                <div class="program-to">
+                    December 2014
+                </div>
+            </div>
+        </strong>
+    </div>
+    <div class="program-links">
+        <a href="https://docs.google.com/gview?url=http://chirojongens.chiroelzestraat.be/uploads/ProgramSeptemberDecember2014.pdf?embedded=true"
+           onclick="return popup(this, 'notes')">Bekijken</a>
+        <a href="uploads/ProgramSeptemberDecember2014.pdf">Downloaden</a>
+    </div>
 </div>
 <script type="text/javascript">
     <!--
