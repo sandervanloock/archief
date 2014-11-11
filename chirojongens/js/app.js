fe8557e7a96b02ddc1eeea900efb3cd57b1cc273
Sander Van Loock <svl@foreach.be>
2014-11-11 16:45:01 +0100

    5 pages

--- chirojongens/js/app.js
+++ chirojongens/js/app.js
@@ -1,25 +1,25 @@
 'use strict';
 
-var app = angular.module('app', ['ngRoute'], function($locationProvider) {
+var app = angular.module('app', ['ngRoute'], function ($locationProvider) {
     $locationProvider.html5Mode(true);
 });
 
-app.controller('NavController',function($scope){
+app.controller('NavController', function ($scope, $location) {
     console.log('NavController init');
-    $scope.currentPage = 'home';
+    $scope.currentPage = $location.url().substring(13);
 });
 
-app.controller('GroupNavController',function($scope){
+app.controller('GroupNavController', function ($scope) {
     console.log('GroupNavController init');
     $scope.currentGroup = 'speelclub';
 });
 
-app.controller('GroupController',function($scope,$routeParams){
+app.controller('GroupController', function ($scope, $routeParams) {
     console.log('GroupController init');
-    console.log('found',$routeParams,'in URL');
+    console.log('found', $routeParams, 'in URL');
 });
 
-app.controller('GeneralCalendarController',function($scope){
+app.controller('GeneralCalendarController', function ($scope) {
     console.log('GeneralCalendarController init');
 })
 
@@ -65,14 +65,31 @@ app.constant('Constants',
 );
 
 
-app.config(function($routeProvider){
+app.config(function ($routeProvider) {
     $routeProvider
-        .when('/chirojongens/',{
-            templateUrl: 'templates/home.tpl.html'
+        .when('/chirojongens/', {
+            templateUrl: 'templates/home.tpl.html',
+            controller: 'NavController'
         })
-        .when('/chirojongens/groepen/:groupname',{
+        .when('/chirojongens/groepen/:groupname', {
             templateUrl: '/chirojongens/templates/groepen.tpl.html',
-           controller: 'GroupController'
+            controller: 'GroupController'
+        })
+        .when('/chirojongens/overons', {
+            templateUrl: '/chirojongens/templates/overons.tpl.html',
+            controller: 'NavController'
+        })
+        .when('/chirojongens/media', {
+            templateUrl: '/chirojongens/templates/media.tpl.html',
+            controller: 'NavController'
+        })
+        .when('/chirojongens/verhuur', {
+            templateUrl: '/chirojongens/templates/verhuur.tpl.html',
+            controller: 'NavController'
+        })
+        .when('/chirojongens/contact', {
+            templateUrl: '/chirojongens/templates/contact.tpl.html',
+            controller: 'NavController'
         })
         .otherwise({
             template: 'No route matches'
