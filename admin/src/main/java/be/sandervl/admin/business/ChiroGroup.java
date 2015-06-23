914c4983502d5fee203e029c6a93c2d4b9f5f8c6
Sander Van Loock <svl@foreach.be>
2015-06-23 09:03:14 +0200

    correct group calendar ids

--- admin/src/main/java/be/sandervl/admin/business/ChiroGroup.java
+++ admin/src/main/java/be/sandervl/admin/business/ChiroGroup.java
@@ -1,11 +1,11 @@
 package be.sandervl.admin.business;
 
 public enum ChiroGroup {
-    SPEELCLUB("52on50vspdubfmmbdenbamr0k0@group.calendar.google.com"),
-    RAKKERS("ofnonpgnb4affnd6t51e48fsdo@group.calendar.google.com"),
-    TOPPERS("80k5sp0pj8973vrdj5lafga33g@group.calendar.google.com"),
-    KERELS("vtuadhst93b1h7879gogidqepk@group.calendar.google.com"),
-    ASPIRANTEN("25bpqoqdk4j0a6p4m68uqea3ig@group.calendar.google.com"),
+    SPEELCLUB("ofnonpgnb4affnd6t51e48fsdo@group.calendar.google.com"),
+    RAKKERS("80k5sp0pj8973vrdj5lafga33g@group.calendar.google.com"),
+    TOPPERS("vtuadhst93b1h7879gogidqepk@group.calendar.google.com"),
+    KERELS("25bpqoqdk4j0a6p4m68uqea3ig@group.calendar.google.com"),
+    ASPIRANTEN("9aomc83dom4mbdq10m5t8vnkpo@group.calendar.google.com"),
     LEIDING("");
 
     private String calendarId;
